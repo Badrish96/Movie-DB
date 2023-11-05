e97ca386f174c30f2cde58711eb17974aa2a633d
Badrish96 <badrishchoubeystar@gmail.com>
2023-11-05 14:07:30 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import Routes
+import { Route, Routes } from "react-router-dom"; // Import Routes
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NewReleases from "./components/NewReleases";
@@ -8,14 +8,14 @@ import WhatsHot from "./components/WhatsHot";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/new" element={<NewReleases />} />
         <Route path="/hot" element={<WhatsHot />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
